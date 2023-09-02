be5c5a079e1afff180bed9d82f7f4be2e16fee1f
pippo1114 <143859363+pippo1114@users.noreply.github.com>
2023-09-02 10:43:00 +0700

    commit1

---
+++ rnt1/rentet1.py
@@ -0,0 +1,26 @@
+# # apel = 52.1
+# # print(apel)
+# # print (type (apel))
+# # apel2 = 52
+# # print(type(apel2))
+# # nama_apel = "apel"
+# # print (nama apel)
+# # print (type (nama apel))
+# # nama_apel =
+# # a1pel =
+
+# # list_warna_apel = ['hijau', 'merah']
+# # list_apel1 =['manis', 2 , 3.09]
+# # print(list_warna_apel )
+# # print(list_apel1)
+
+# # tuple_jumlah = (2, .32, "apel")
+# # print (tuple_jumlah)
+
+# apel_dict = {'nama' : 'apel', 'jumlah' : '3' }
+# print = (apel_dict['nama'])
+# print = (apel_dict['jumlah'])
+
+a = 96
+b = 2
+print (a ** b)
