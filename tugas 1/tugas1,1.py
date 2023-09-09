f58dd8d804fbf7c89bd2e75a773c9746bd34901d
pippo1114 <143859363+pippo1114@users.noreply.github.com>
2023-09-10 01:35:56 +0700

    Add files via upload
    penugasan dengan nim ganjil nomer 1 dan 3

---
+++ tugas 1/tugas1,1.py
@@ -0,0 +1,5 @@
+celcius = float(input("tuliskan suhu dalam celcius: "))
+
+fahrenheit = (1.8 * celcius) + 32
+
+print ( "sama dengan", (fahrenheit) ,"derajat farenheit")
---
+++ tugas 1/tugas1,3.py
@@ -0,0 +1,8 @@
+print ("menggunakan program mengkonversi suhu ")
+
+kelvin = float(input("tuliskan suhu dalam kelvin: "))
+
+celcius = ( kelvin-273)
+
+print ( "sama dengan:" , ( celcius ), 'derajat celcius')
+
